d2b86b0edc32d1fa62ab3eea222441568c0283ce
sparshmahajan <sparshmahajan3@gmail.com>
2023-01-31 02:30:59 +0530

    fixation

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import logo from './logo.svg';
+import './App.css';
+
+export default function App() {
+  return (
+    <h1 className="text-3xl font-bold text-blue-500 underline text-center">
+      Welcome to React Tailwind CSS
+    </h1>
+  )
+}
--- src/components/App.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-
-function App(){
-    return (
-        <h1>From app</h1>
-    )
-}
-export default App;
