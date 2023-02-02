438148990741639d473323a16b162350212a015b
ayaz <ayaz11ansari@gmail.com>
2023-02-02 16:42:02 +0530

    article/blog

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Top from "./top";
 import Footer from "./Footer";
+import Blog from './Blog';
 function App(){
 
     return (
         <div>
        <Top/>
        <Footer/>
+       <Blog/>
         </div>
     )
 
